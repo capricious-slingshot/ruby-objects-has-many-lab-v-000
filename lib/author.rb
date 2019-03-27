3c978898e704c9abea43501a026d84846c3616f5
Carden Z <ephemere@protonmail.com>
2019-03-27 18:24:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Author
-  @@all = []
+  # @@all = []
   attr_reader :name
   attr_accessor :posts
 
@@ -13,17 +13,17 @@ class Author
   def add_post(new_post)
     @posts << new_post
     new_post.author = self
-    @@all << new_post
+    # @@all << new_post
   end
 
   def add_post_by_title(title)
     new_post = Post.new(title)
     new_post.author = self
     @posts << new_post
-    @@all << new_post
+    # @@all << new_post
   end
 
   def self.post_count
-    @@all.length
+    # @@all.length
   end
 end
