e3203d32f942868fcc191b695159958c95ce6fdd
Carden Z <ephemere@protonmail.com>
2019-03-27 18:23:59 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Author
+  @@all = []
+  attr_reader :name
+  attr_accessor :posts
+
+  def initialize(name)
+    @name  = name
+    @posts = []
+  end
+
+  def add_post(new_post)
+    @posts << new_post
+    new_post.author = self
+    @@all << new_post
+  end
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self
+    @posts << new_post
+    @@all << new_post
+  end
+
+  def self.post_count
+    @@all.length
+  end
+end
